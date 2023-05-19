86f79b6880635ba7f2824bc5d78ff6fe46b45bd0
tenthyoung <tenthyoung@gmail.com>
2023-05-18 17:30:11 -0700

    feat: create UI (wip)

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import {
+  Button,
+  Container,
+  FormLabel,
+  Heading,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fret, setFret] = React.useState(undefined);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container textAlign="left">
+        <Heading as="h1" size="2xl" mb="6">
+          Capo Chord Converter
+        </Heading>
+        <Text mb="8">
+          Paste your tabs into the text area. Choose the fret you want to capo
+          on, and it will be converted below
+        </Text>
+        {/* /* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* register your input into the hook by invoking the "register" function */}
+          <FormLabel size="lg" fontWeight={"bold"}>
+            Chords in the song:
+          </FormLabel>
+          <Input mb="5" placeholder="B G#m C Em" {...register("example")} />
+
+          {/* include validation with required or other standard HTML validation rules */}
+          <FormLabel size="lg" fontWeight={"bold"}>
+            What shape do you want to play?
+          </FormLabel>
+          <Input {...register("exampleRequired", { required: true })} />
+          {/* errors will return when field validation fails  */}
+          {errors.exampleRequired && <span>This field is required</span>}
+
+          <Button mt="8" type="submit">
+            Submit
+          </Button>
+        </form>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
